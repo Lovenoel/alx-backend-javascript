ad73f5346a1ca84be02867332c14b6fb715db7d4
lovenoel <kinglovenoel@gmail.com>
2024-10-03 02:06:05 +0300

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -3,11 +3,8 @@ A file that Returns a Promise using this prototype
 function getResponseFromAPI()
 */
 
-const getResponseFromAPI = () => {
-    return new Promise((resolve, reject) => {
-    {
-        resolve();
-    }
-})
+function getFullResponseFromAPI() {
+    return new Promise((resolve, reject) => {})
 }
-export default getResponseFromAPI;
+
+export default getFullResponseFromAPI
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+/*
+A file that uses the prototype below to return a promise.
+The parameter is a boolean.
+getFullResponseFromAPI(success)
+*/
+
+import { resolve } from "styled-jsx/css"
+
+const getFullResponseFromAPI = (success) => {
+    return new Promise((resolve, reject) => {
+        const success = true
+        
+    })
+}
