1c884496c98bbefc224787546e6471d063756ba2
lovenoel <kinglovenoel@gmail.com>
2024-10-03 04:25:34 +0300

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,8 @@
 A function named handleProfileSignup that accepts three arguments
 firstName (string), lastName (string), and fileName (string)
 */
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
