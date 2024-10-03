b3b8ae4fdccf05058fd5c57cd2cdada29c0e8368
lovenoel <kinglovenoel@gmail.com>
2024-10-03 03:45:55 +0300

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
-/*
-A file that Returns a Promise using this prototype
-function getResponseFromAPI()
-*/
-
-function getFullResponseFromAPI() {
-    return new Promise((resolve, reject) => {})
+export default function getFullResponseFromAPI() {
+    return Promise.resolve();
 }
-
-export default getFullResponseFromAPI
