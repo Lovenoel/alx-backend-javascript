071f0f2b1ec4a10483783b0df117f8cf52f7e30a
lovenoel <kinglovenoel@gmail.com>
2024-10-03 03:02:42 +0300

    Task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,28 @@
+/*
+Use the function prototype below
+
+function handleResponseFromAPI(promise)
+Append three handlers to the function:
+
+When the Promise resolves, return an object with the following attributes
+status: 200
+body: success
+When the Promise rejects, return an empty Error object
+For every resolution, log Got a response from the API to the console
+*/
+
+const handleResponseFromAPI = (promise) => {
+    return promise
+    .then(() => {
+        console.log('Got a response from API')
+        return {
+            status : 200,
+            body : 'Success',
+        };
+    })
+    .catch(() => {
+        return new error();
+    });
+};
+
+export default handleResponseFromAPI;
