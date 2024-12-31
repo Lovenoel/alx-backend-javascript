263043966e3464ae3410a911bdad855bb68d8d3a
lovenoel <kinglovenoel@gmail.com>
2024-12-31 11:47:28 +0000

    Added tasks 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,23 +1,23 @@
 export default function createIteratorObject(report) {
-  const employees = [];
+  if (!report || !report.allEmployees) {
+    throw new Error('Invalid report object');
+  }
 
-  // Flatten the employee list from all departments
+  const employees = [];
   for (const department of Object.values(report.allEmployees)) {
     employees.push(...department);
   }
 
-  // Return an iterator
   return {
     [Symbol.iterator]() {
       let index = 0;
       return {
         next() {
-          if (index < employees.length) {
-            const value = employees[index];
-            index += 1; // Replaced index++ with index += 1
-            return { value, done: false };
-          }
-          return { done: true };
+          const employee = employees[index];
+          index += 1; // Avoids using 'index++'
+          return employee !== undefined
+            ? { value: employee, done: false }
+            : { done: true };
         },
       };
     },
