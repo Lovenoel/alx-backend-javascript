325afd7e13a073f8871f566dd734c1288e35d385
lovenoel <kinglovenoel@gmail.com>
2024-12-31 08:31:18 +0000

    added fils

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true; // Block-scoped variable
+    const task2 = false; // Block-scoped variable
   }
 
   return [task, task2];
